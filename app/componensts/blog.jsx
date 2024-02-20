8dba000ee5277431d068b07fa2d1eb7df49f4a7b
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-02-20 10:45:15 +0000

    responsive code added

--- app/componensts/blog.jsx
+++ app/componensts/blog.jsx
@@ -1,102 +1,121 @@
-"use client"
+"use client";
 import styles from "../home.module.scss";
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 export default function Blog() {
-    return (
-        <>
-
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
-                className={styles.blogs}
-            >
-                <SwiperSlide>
-                    <div className={styles.blog}>
-                        <div className={styles.imgContainer}>
-                            <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="" />
-                        </div>
-                        <div className={styles.content}>
-                            <p className={styles.metadata}>
-                                22 Oct, 2020
-                            </p>
-                            <p className={styles.description}>
-                                Lorem ipsum dolor sit consea. Nulla purus arcu
-                            </p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className={styles.blog}>
-                        <div className={styles.imgContainer}>
-                            <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="" />
-                        </div>
-                        <div className={styles.content}>
-                            <p className={styles.metadata}>
-                                22 Oct, 2020
-                            </p>
-                            <p className={styles.description}>
-                                Lorem ipsum dolor sit consea. Nulla purus arcu
-                            </p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide><div className={styles.blog}>
-                    <div className={styles.imgContainer}>
-                        <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt="" />
-                    </div>
-                    <div className={styles.content}>
-                        <p className={styles.metadata}>
-                            22 Oct, 2020
-                        </p>
-                        <p className={styles.description}>
-                            Lorem ipsum dolor sit consea. Nulla purus arcu
-                        </p>
-                    </div>
-                </div></SwiperSlide>
-                <SwiperSlide><div className={styles.blog}>
-                    <div className={styles.imgContainer}>
-                        <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt="" />
-                    </div>
-                    <div className={styles.content}>
-                        <p className={styles.metadata}>
-                            22 Oct, 2020
-                        </p>
-                        <p className={styles.description}>
-                            Lorem ipsum dolor sit consea. Nulla purus arcu
-                        </p>
-                    </div>
-                </div></SwiperSlide>
-                <SwiperSlide><div className={styles.blog}>
-                    <div className={styles.imgContainer}>
-                        <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt="" />
-                    </div>
-                    <div className={styles.content}>
-                        <p className={styles.metadata}>
-                            22 Oct, 2020
-                        </p>
-                        <p className={styles.description}>
-                            Lorem ipsum dolor sit consea. Nulla purus arcu
-                        </p>
-                    </div>
-                </div></SwiperSlide>
-            </Swiper>
-        </>
-    )
+  return (
+    <>
+      <Swiper
+        // slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className={styles.blogs}
+      >
+        <SwiperSlide>
+          <div className={styles.blog}>
+            <div className={styles.imgContainer}>
+              <img
+                src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <p className={styles.metadata}>22 Oct, 2020</p>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit consea. Nulla purus arcu
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.blog}>
+            <div className={styles.imgContainer}>
+              <img
+                src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <p className={styles.metadata}>22 Oct, 2020</p>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit consea. Nulla purus arcu
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.blog}>
+            <div className={styles.imgContainer}>
+              <img
+                src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <p className={styles.metadata}>22 Oct, 2020</p>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit consea. Nulla purus arcu
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.blog}>
+            <div className={styles.imgContainer}>
+              <img
+                src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <p className={styles.metadata}>22 Oct, 2020</p>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit consea. Nulla purus arcu
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.blog}>
+            <div className={styles.imgContainer}>
+              <img
+                src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <p className={styles.metadata}>22 Oct, 2020</p>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit consea. Nulla purus arcu
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
--- app/componensts/testimonial.jsx
+++ app/componensts/testimonial.jsx
@@ -1,52 +1,66 @@
-"use client"
+"use client";
 import styles from "../home.module.scss";
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 export default function Testimonial() {
-    return (
-        <>
-
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={1}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
-                className={styles.reviews}
-            >
-                <SwiperSlide>
-                    <div className={styles.review}>
-                        <p>“Nulla efficitur nisl sit amet velit malesuada dapibus. Duis mollis felis turpis, nec semper odio convallis at. Curabitur imperdiet semper arcu, a finibus arcu suscipit in. Donec quis placerat nibh. Maecenas est purus, eleifend ac cursus sed, tincidunt ut sapien.
-                            Morbi ornare elit at libero suscipit porta.”</p>
-                        <h4>Esther Howard</h4>
-                        <span>Managing Director, ABC company</span>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className={styles.review}>
-                        <p>“Nulla efficitur nisl sit amet velit malesuada dapibus. Duis mollis felis turpis, nec semper odio convallis at. Curabitur imperdiet semper arcu, a finibus arcu suscipit in. Donec quis placerat nibh. Maecenas est purus, eleifend ac cursus sed, tincidunt ut sapien.
-                            Morbi ornare elit at libero suscipit porta.”</p>
-                        <h4>Esther Howard</h4>
-                        <span>Managing Director, ABC company</span>
-                    </div>
-                </SwiperSlide>     <SwiperSlide>
-                    <div className={styles.review}>
-                        <p>“Nulla efficitur nisl sit amet velit malesuada dapibus. Duis mollis felis turpis, nec semper odio convallis at. Curabitur imperdiet semper arcu, a finibus arcu suscipit in. Donec quis placerat nibh. Maecenas est purus, eleifend ac cursus sed, tincidunt ut sapien.
-                            Morbi ornare elit at libero suscipit porta.”</p>
-                        <h4>Esther Howard</h4>
-                        <span>Managing Director, ABC company</span>
-                    </div>
-                </SwiperSlide>
-
-            </Swiper>
-        </>
-    )
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className={styles.reviews}
+      >
+        <SwiperSlide>
+          <div className={styles.review}>
+            <p>
+              “Nulla efficitur nisl sit amet velit malesuada dapibus. Duis
+              mollis felis turpis, nec semper odio convallis at. Curabitur
+              imperdiet semper arcu, a finibus arcu suscipit in. Donec quis
+              placerat nibh. Maecenas est purus, eleifend ac cursus sed,
+              tincidunt ut sapien. Morbi ornare elit at libero suscipit porta.”
+            </p>
+            <h3>Esther Howard</h3>
+            <span>Managing Director, ABC company</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.review}>
+            <p>
+              “Nulla efficitur nisl sit amet velit malesuada dapibus. Duis
+              mollis felis turpis, nec semper odio convallis at. Curabitur
+              imperdiet semper arcu, a finibus arcu suscipit in. Donec quis
+              placerat nibh. Maecenas est purus, eleifend ac cursus sed,
+              tincidunt ut sapien. Morbi ornare elit at libero suscipit porta.”
+            </p>
+            <h3>Esther Howard</h3>
+            <span>Managing Director, ABC company</span>
+          </div>
+        </SwiperSlide>{" "}
+        <SwiperSlide>
+          <div className={styles.review}>
+            <p>
+              “Nulla efficitur nisl sit amet velit malesuada dapibus. Duis
+              mollis felis turpis, nec semper odio convallis at. Curabitur
+              imperdiet semper arcu, a finibus arcu suscipit in. Donec quis
+              placerat nibh. Maecenas est purus, eleifend ac cursus sed,
+              tincidunt ut sapien. Morbi ornare elit at libero suscipit porta.”
+            </p>
+            <h3>Esther Howard</h3>
+            <span>Managing Director, ABC company</span>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
