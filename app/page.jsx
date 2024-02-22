591f4b2460bbf67861e5769b9279aecef6b46497
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-02-22 06:52:04 +0000

    added 3 projects with link

--- app/page.jsx
+++ app/page.jsx
@@ -75,7 +75,10 @@ export default function Home() {
                 />
                 <div className={styles.social}>
                   <div className={styles.box}>
-                    <a href="https://linkedin.com/curiousdilip" target="_blank">
+                    <Link
+                      href="https://linkedin.com/in/curiousdilip"
+                      target="_blank"
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -86,8 +89,11 @@ export default function Home() {
                       >
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                       </svg>
-                    </a>
-                    <a href="https://github.com/curiousdilip" target="_blank">
+                    </Link>
+                    <Link
+                      href="https://github.com/curiousdilip"
+                      target="_blank"
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -98,8 +104,11 @@ export default function Home() {
                       >
                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                       </svg>
-                    </a>
-                    <a href="https://twitter.com/curiousdilip" target="_blank">
+                    </Link>
+                    <Link
+                      href="https://twitter.com/curiousdilip"
+                      target="_blank"
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -110,8 +119,8 @@ export default function Home() {
                       >
                         <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
                       </svg>
-                    </a>
-                    <a href="https://www.dilipmaurya.in" target="_blank">
+                    </Link>
+                    <Link href="https://www.dilipmaurya.in" target="_blank">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -122,7 +131,7 @@ export default function Home() {
                       >
                         <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
                       </svg>
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -143,23 +152,10 @@ export default function Home() {
                 </p>
                 <div className={styles.ctaBtns}>
                   <button>
-                    <a href="#">My Projects</a>
+                    <Link href="#portfolio">My Projects</Link>
                   </button>
                   <button>
-                    <a href="#">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-download"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
-                      </svg>
-                      Download CV
-                    </a>
+                    <Link href="#">Download CV</Link>
                   </button>
                 </div>
               </div>
@@ -271,16 +267,23 @@ export default function Home() {
                   />
                 </div>
                 <div className={styles.content}>
-                  <p className={styles.type}>UI UX </p>
-                  <h3>Somthing</h3>
+                  <p className={styles.type}>
+                    Nextjs, CI/CD Pipeline, Google Analytics{" "}
+                  </p>
+                  <h3>Ramen Paul</h3>
                   <div className={styles.description}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Doloribus quas dolores quidem dolorem nostrum expedita
-                    autem, est rerum voluptate alias ex facere repellat esse eos
-                    ipsam exercitationem iusto repudiandae laudantium.
+                    Ramen Paul, my music teacher, inspired my journey into web
+                    development. I crafted his initial website, showcasing his
+                    musical expertise. Motivated to enhance my skills, I learned
+                    React and subsequently revamped his site using Next.js. Now,
+                    it not only reflects his artistry but also highlights my
+                    growth as a developer, harmonizing technology and creativity
+                    to amplify his online presence.
                   </div>
                   <button>
-                    <a href="#">Case Study</a>
+                    <Link href="https://www.ramenpaul.com" target="_blank">
+                      Visit
+                    </Link>
                   </button>
                 </div>
               </div>
@@ -292,17 +295,20 @@ export default function Home() {
                   />
                 </div>
                 <div className={styles.content}>
-                  <p className={styles.type}>UI UX </p>
-                  <h3>Somthing</h3>
+                  <p className={styles.type}>Wordpress, VPS </p>
+                  <h3>MPAA - Performing Arts Academy</h3>
 
                   <div className={styles.description}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Doloribus quas dolores quidem dolorem nostrum expedita
-                    autem, est rerum voluptate alias ex facere repellat esse eos
-                    ipsam exercitationem iusto repudiandae laudantium.
+                    Collaborating with Sahil Bansal, I tackled my first global
+                    project—creating the MPAA - Performing Arts Academy website
+                    using WordPress. Highlighting a BA (Hons) Degree in Musical
+                    Theatre Performance with impressive employment outcomes, the
+                    site faced challenges like malware attacks. I gained skills
+                    in VPS, mail setup, and recovery. Now, I handle monthly
+                    maintenance, while Sahil Bansal manages marketing and ads.
                   </div>
                   <button>
-                    <a href="#">Case Study</a>
+                    <Link href="https://mpaa.ac.uk/">Visit</Link>
                   </button>
                 </div>
               </div>
@@ -314,17 +320,19 @@ export default function Home() {
                   />
                 </div>
                 <div className={styles.content}>
-                  <p className={styles.type}>UI UX </p>
-                  <h3>Somthing</h3>
+                  <p className={styles.type}>HTML, CSS, JS </p>
+                  <h3>Rainbow Consultancy</h3>
 
                   <div className={styles.description}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Doloribus quas dolores quidem dolorem nostrum expedita
-                    autem, est rerum voluptate alias ex facere repellat esse eos
-                    ipsam exercitationem iusto repudiandae laudantium.
+                    Rainbow Consultancy is a consortium of independent
+                    professional consultants with a commitment to provide
+                    quality training, evaluation and documentation services to
+                    our clients. I had the chance to make website for them.
                   </div>
                   <button>
-                    <a href="#">Case Study</a>
+                    <Link href="https://rainbowconsultancy.in/" target="_blank">
+                      Visit
+                    </Link>
                   </button>
                 </div>
               </div>
